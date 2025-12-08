279070f50955b9dff7ef9eaec2843d848ad761b4
Mina Radi <Mina.Radi@linkdev.com>
2025-12-08 23:57:15 +0200

    Add MediatR support and new BuildingBlocks project
    - Updated `Catalog.API.csproj` to include `MediatR` and Azure Containers Tools.
    - Modified `launchSettings.json` to change application URLs.
    - Added new `BuildingBlocks` project with `BuildingBlocks.csproj`.
    - Created `ICommand` and `IQuery` interfaces for CQRS.
    - Introduced `Product` model class with relevant properties.
    - Created `CreateProductEndPoint` class (no functionality yet).
    - Added `CreateProductCommand`, `CreateProductResult`, and handler in `CreateProductHandler.cs`.

---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/ICommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+
+namespace BuildingBlocks.CQRS;
+
+// Icmmand without response
+public interface ICommand : ICommand<Unit>
+{
+}
+// Icommand with response
+public interface ICommand<out TResponse> : IRequest<TResponse>
+{
+}
+
